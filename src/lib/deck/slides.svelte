2ff36b739b08e318d71202318c62ac7563975ee8
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-07 13:58:43 +0200

    The Slide Deck Done

---
+++ src/lib/deck/slides.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    import Reveal from "reveal.js";
+    import Markdown from 'reveal.js/plugin/markdown/markdown'
+    import Highlight from 'reveal.js/plugin/highlight/highlight'
+    import Notes from 'reveal.js/plugin/notes/notes'
+
+    import 'reveal.js/dist/reset.css'
+    import 'reveal.js/dist/theme/black.css'
+    import 'reveal.js/plugin/highlight/monokai.css'
+
+    onMount(() => {
+        const deck = new Reveal({
+            plugins: [Markdown, Highlight, Notes],
+            autoAnimateEasing: 'ease',
+            autoAnimateDuration: 1,
+            hash: true,
+            // controls: true,
+            // progress: true,
+        });
+
+        deck.initialize();
+    })
+</script>
+
+<div class="reveal">
+    <div class="slides">
+        <section>Slide 1</section>
+        <section>Slide 2</section>
+    </div>
+</div>
